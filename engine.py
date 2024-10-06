7f38ae1ed52211497386b10c43bd0fd8f73fe2f6
Omena0 <Omena0MC@gmail.com>
2024-10-06 23:28:49 +0300

    work

--- engine.py
+++ engine.py
@@ -1,56 +1,160 @@
 import pygame
+import math
+
+def clamp(num, min_=0, max_=255):
+    return min(max(num, min_), max_)
+
+def clamp_rgb(col):
+    return (clamp(col[0]), clamp(col[1]), clamp(col[2]))
 
 class Sprite:
     def __init__(self, pos, texture):
         self.pos = pos
+        self.x = pos[0]
+        self.y = pos[1]
+        self.width = len(texture)+1
+        self.height = len(texture[0])+1
         self.texture = texture
 
-    def move(self, pos):
+    def setPos(self,pos):
         self.pos = pos
-    
+        self.x = pos[0]
+        self.y = pos[1]
+
+    def move(self, pos):
+        self.x += pos[0]
+        self.y += pos[1]
+        self.pos = self.x, self.y
+
+    def collides_with(self, sprites):
+        if isinstance(sprites, list):
+            return [sprite for sprite in sprites if self.collides_with(sprite)]
+
+        if sprites == "edge":
+            return (
+                self.x < 0
+                or self.y < 0
+                or self.x + self.width > self.game.width
+                or self.y-1 + self.height > self.game.height
+            )
+
+        return sprites if (
+            self.x-1 + self.width > sprites.x
+            and self.x+1 < sprites.x + sprites.width
+            and self.y-1 + self.height > sprites.y
+            and self.y+1 < sprites.y + sprites.height
+        ) else None
+
+    def raycast(self, angle, distance=500):
+        """
+        Raycast from the sprite in the given direction
+
+        Returns the distance to the first collision, or None if no collision is found
+        """
+        angle = math.radians(angle)
+        dx = round(math.cos(angle))
+        dy = round(math.sin(angle))
+        x = self.x+self.width/2
+        y = self.y+self.height/2
+
+        for steps in range(distance):
+            x += dx
+            y += dy
+            for sprite in self.game.sprites:
+                if sprite != self and sprite.x <= x < sprite.x + sprite.width and sprite.y <= y < sprite.y + sprite.height:
+                    return steps, sprite
+
+        return None
+
+
     def add(self, game):
         self.game = game
-        game.sprites.add(self)
+        game.sprites.append(self)
+        return self
+
+events = {
+    "keyDown": pygame.KEYDOWN,
+    "keyUp": pygame.KEYUP
+}
 
 class Game:
-    def __init__(self, title, size, res=16, max_fps=0):
+    def __init__(self, title, size, res=16, max_fps=0, bg=(0,0,0)):
         self.title = title
         self.size = size
+        self.width = size[0]
+        self.height = size[1]
         self.res = res
         self.max_fps = max_fps
+        self.bg = bg
 
-        self.regions = [set() for _ in range(16)]
+        self.sprites = []
+        self.shaders = []
+        self.events = {}
 
-        self.screen = pygame.display.set_mode(size)
+        self.use_shader_rendering = False
+
+        self.screen = pygame.display.set_mode((self.width*res,self.height*res))
         self.clock = pygame.time.Clock()
 
-    def _shader(self,x,y):
-        # The entire window will be split into regions that each contain sprites.
-        # Loop for each sprite in the region
-        
-
-
-        col = (0,0,0)
-        pygame.draw.rect(self.screen, col, (x*self.res, y*self.res, self.res, self.res))
-
     def _draw(self):
-        for y in range(self.size[0]//self.res):
-            for x in range(self.size[1]//self.res):
-                self._shader(x,y)
+        col = self.bg
+
+        if self.shaders:
+            for y in range(self.size[1]):
+                for x in range(self.size[0]):
+                    for shader in self.shaders:
+                        col = shader(col, x, y, self.frame, None)
+                        if not col: return
+
+                    pygame.draw.rect(self.screen, clamp_rgb(col), (x*self.res, y*self.res, self.res, self.res))
+
+        for sprite in self.sprites:
+            for y in range(sprite.height-1):
+                for x in range(sprite.width-1):
+                    col = sprite.texture[x][y]
+                    for shader in self.shaders:
+                        col = shader(col, sprite.x+x, sprite.y+y, self.frame, sprite)
+                        if not col: return
+
+                    pygame.draw.rect(self.screen, clamp_rgb(col), ((sprite.x+x)*self.res, (sprite.y+y)*self.res, self.res, self.res))
+
+
+    def shader(self,callback):
+        self.shaders.append(callback)
+
+    def on(self,action):
+        print(f'Registered Event: {action}')
+        def inner(callback):
+            if action[0] not in self.events:
+                self.events[action] = []
+
+            self.events[action].append((callback))
+
+        return inner
 
     def run(self):
         self.running = True
+        self.frame = 0
         while self.running:
             for event in pygame.event.get():
+                for action,callbacks in self.events.items():
+                    for eventName, eventValue in events.items():
+                        if action == eventName and event.type == eventValue:
+                            for callback in callbacks:
+                                callback(event.dict['key'])
+
                 if event.type == pygame.QUIT:
                     self.running = False
-                    pygame.quit()
 
             self._draw()
+
+            for callback in self.events.get("frame",[]):
+                callback(self.frame)
 
             pygame.display.set_caption(f'{self.title} FPS: {round(self.clock.get_fps(),2)}')
             pygame.display.update()
 
+            self.frame += 1
             self.clock.tick(self.max_fps)
 
 
--- main.py
+++ main.py
@@ -1,6 +1,148 @@
 import engine as gl
 
-game = gl.Game("Game", (800, 600)).run()
+jumpForce = 1.2
+dashForce = 1.5
+gravity = 0.03
+acceleration = 30
+max_speed = 0.7
+air_resistance = 0.97
+maxJumps = 2
+maxDashes = 1
 
+# Initialize the game
+game = gl.Game("fr", (75,50),res=8,max_fps=120)
+
+cx,cy = 0,0
+
+# Add a player sprite
+player_texture = [
+    [(255, 0, 0), (255, 0, 0), (255, 0, 0)],
+    [(255, 0, 0), (255, 0, 0), (255, 0, 0)],
+    [(255, 0, 0), (255, 0, 0), (255, 0, 0)]
+]
+
+player = gl.Sprite((game.width // 2, game.height // 3*2), player_texture).add(game)
+
+platforms = []
+
+class Platform():
+    def __init__(self, pos, width, height=3, attributes=None, shader = None):
+        if attributes is None:
+            attributes = {"bounciness": 1}
+
+        self.pos = pos
+        self.x = pos[0]
+        self.y = pos[1]
+        self.width = width
+        self.height = height
+        self.texture = [[shader() if shader else (255,255,255) for _ in range(self.height)] for _ in range(self.width)]
+        self.sprite = gl.Sprite(self.pos, self.texture).add(game)
+
+        for key,value in attributes.items():
+            setattr(self.sprite, key, value)
+
+        platforms.append(self.sprite)
+
+
+Platform((35, 40), 50)
+Platform((100, 30), 50)
+Platform((60, 50), 50)
+
+
+
+
+def updateCamera():
+    global cx,cy
+    if cy < -50:
+        cy = -cy-30
+    for sprite in game.sprites:
+        if sprite == player:
+            continue
+        sprite.x = sprite.pos[0] + cx
+        sprite.y = sprite.pos[1] + cy
+
+@game.on('frame')
+def frame(frame):
+    global cx,cy, pressed, jumps, dashes
+
+    vel[0] = round(vel[0],10)
+    vel[1] = round(vel[1],10)
+
+    vel[0] = vel[0]*air_resistance
+    vel[1] = vel[1]*air_resistance
+
+
+    cx += vel[0]
+    cy += vel[1]
+
+    if abs(vel[0]) < max_speed or vel[0] * pressed < 0:
+        vel[0] += pressed/acceleration
+
+    if platform := player.collides_with(platforms):
+        jumps = maxJumps
+        dashes = maxDashes
+        platform = platform[0]
+        vel[1] *= -platform.bounciness
+        vel[1] = int(vel[1])
+
+        while player.collides_with(platforms):
+            cy += 0.01
+            updateCamera()
+
+        cy -= 0.015
+
+    else:
+        vel[1] -= gravity
+
+    updateCamera()
+    if frame % 10 == 0:
+        print(f'X: {round(cx):<5} Y: {round(cy):<5} VX: {round(vel[0]):<5} VY: {round(vel[1]):<5}')
+
+vel = [0,0]
+pressed = 0
+
+@game.on('keyDown')
+def move(key):
+    global cx, cy, pressed, jumps, dashes
+    if key == gl.pygame.K_w:
+        if jumps:
+            jumps -= 1
+            vel[1] += jumpForce
+            cy += vel[1]
+            updateCamera()
+
+    elif key == gl.pygame.K_SPACE:
+        if dashes:
+            dashes -= 1
+            if vel[0] > 0:
+                vel[0] += dashForce
+            else:
+                vel[0] -= dashForce
+
+            print(vel)
+            updateCamera()
+
+    elif key == gl.pygame.K_a:
+        pressed += 1
+
+    elif key == gl.pygame.K_d:
+        pressed -= 1
+
+    elif key == gl.pygame.K_q:
+        game.running = False
+
+@game.on('keyUp')
+def move(key):
+    global cx, cy, pressed
+    if key == gl.pygame.K_a:
+        pressed -= 1
+    elif key == gl.pygame.K_d:
+        pressed += 1
+
+# Run the game
 game.run()
+
+
+
+
 
